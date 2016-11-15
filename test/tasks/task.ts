4f66527d39f98213df4a1e4014a31d4de1fbc8b3
zhouhoujun <houjunzhou@skspruce.net>
2016-11-15 16:51:39 +0800

    update doc

--- test/tasks/task.ts
+++ test/tasks/task.ts
@@ -7,7 +7,7 @@ const sourcemaps = require('gulp-sourcemaps');
 let tsProject = ts.createProject('tsconfig.json');
 const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
-import { IPipe, PipeTask, IAssertDist, taskdefine, bindingConfig, IDynamicTaskOption, Operation, ITaskOption, IEnvOption, ITaskContext, ITaskDefine, ITask, ITaskDecorator, ITaskInfo, TaskResult, task, dynamicTask, IDynamicTasks } from '../../src';
+import { IPipe, PipeTask, IAssertDist, taskdefine, bindingConfig, IDynamicTaskOption, Operation, ITaskOption, IEnvOption, ITaskContext, ITaskDefine, ITask, ITaskInfo, TaskResult, task, dynamicTask, IDynamicTasks } from '../../src';
 
 
 @task({
